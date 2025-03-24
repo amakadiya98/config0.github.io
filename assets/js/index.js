b66306679d3d62bd83a2be77d89683201ae39718
amakadiya98 <64594631+amakadiya98@users.noreply.github.com>
2025-03-24 20:33:07 +0530

    Landing page deisgn

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#0796A0',
+        'black': '#000000',
+        'secondary': '#2E2E2E',
+        'primaryDark': '#1C6E75'
+      },
+      fontFamily: {
+        'krub' : 'Krub',
+        'quicksand' : 'Quicksand',
+      },
+      boxShadow: {
+        'custom': '0px 4px 24px 1px #ECF3FA'
+      },
+      screens: {
+        'xl': '1280px',
+        'xs': '520px',
+      },
+    }
+  }
+}
+
+AOS.init({
+  duration: 1500, 
+  offset: 100,   
+  once: true   
+});
+
+
+// header 
+
+ const menuBtn = document.getElementById('mobile-menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+
