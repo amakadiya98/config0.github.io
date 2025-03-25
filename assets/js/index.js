4314894baf60d71b841d36991da8eaa78b636317
amakadiya98 <64594631+amakadiya98@users.noreply.github.com>
2025-03-25 12:23:20 +0530

    add privacy policy and conditions pages are added

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,12 +29,3 @@ AOS.init({
 });
 
 
-// header 
-
- const menuBtn = document.getElementById('mobile-menu-btn');
-  const mobileMenu = document.getElementById('mobile-menu');
-
-  menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-  });
-
